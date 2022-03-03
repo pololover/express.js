ac78f355ee3e5ecfff11a17a95cf1d6bdfb9baaa
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-03-03 20:21:13 +0900

    create 및 update
    파일 생성 및 업데이트 로직 구현

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var template = require('./lib/template.js')
 var path = require('path')
 var sanitizeHtml = require('sanitize-html');
+var qs = require('querystring');
 app.get('/', (req, res) => {
   fs.readdir('./data', function (error, filelist) {  //./data 파일읽고 콜백함수 실행
     var title = 'Welcome';
@@ -30,7 +31,7 @@ app.get('/page/:pageId', function (req, res) { //:로 전달된 부분은 req.pa
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
+                <a href="/update/${sanitizedTitle}">update</a>
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
@@ -40,6 +41,86 @@ app.get('/page/:pageId', function (req, res) { //:로 전달된 부분은 req.pa
     });
   })
 });
+
+app.get('/create', (req, res) => {
+  fs.readdir('./data', function (error, filelist) {
+        var title = 'WEB - create';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
+        res.send(html);
+      });
+})
+
+app.post('/create_process', (req, res) => {
+  var body = '';
+  req.on('data', function (data) {
+    body = body + data;
+  });
+  req.on('end', function () {
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      res.writeHead(302, { Location: `/?id=${title}` });
+      res.end();
+    })
+  });
+});
+
+app.get('/update/:pageId', (req, res) => { // 해당 URL로 이동
+  fs.readdir('./data', function (error, filelist) { //디렉토리를 읽어 list를 만들고
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {  //파일을 읽어 내용을 만든다.
+      var title = filteredId;
+      var list = template.list(filelist);
+      var html = template.HTML(title, list,
+        `
+          <form action="/update_process" method="post">  
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>` 
+      );
+      res.send(html);
+    });
+  })
+});
+
+app.post('/update_process', (req, res) => {
+  var body = '';
+  req.on('data', function(data){
+      body = body + data;
+  });
+  req.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){   //파일의 이름을 바꿔준다.
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){  //파일 내용 갱신.
+          res.writeHead(302, {Location: `page/${title}`});
+          res.end();
+        })
+      });
+  });
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!')) //3000번 포트 등록.
 
