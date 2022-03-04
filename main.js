1d7190b4cf19dfe4c7cde9790a899008e92b2e7f
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-03-05 02:57:23 +0900

    쿠키 인증
    express cookie-Parser 이용. cookie set 및 req.cookies

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression')
 var topicRouter = require('./routes/topic');
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
 app.use(express.static('exam'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(cookieParser());
 
+function authenIs(req, res) {
+  var isOwner = false;
+  if (req.cookies.email == "csh7215@naver.com" && req.cookies.password == "11111") {
+    isOwner = true;
+    return isOwner
+  } else {
+    return isOwner
+  }
+}
 app.get('*', function (request, response, next) {
+  var isOwner = authenIs(request, response);
+  console.log(isOwner);
   fs.readdir('./data', function (error, filelist) {
     request.list = filelist;  
     next();
@@ -20,6 +34,7 @@ app.use('/', indexRouter);
 
 app.use('/topic', topicRouter);
 
+app.use('/login', loginRouter);
 
 
 app.use(function (req, res, next) {
