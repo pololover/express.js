35c3b32e6e544f8d73212b76ab5193f245f18607
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-03-05 00:03:55 +0900

    secure 와 HttpOnly

--- cookie.js
+++ cookie.js
@@ -6,7 +6,15 @@ http.createServer(function (request, response) {
     cookies = cookie.parse(request.headers.cookies)
    }
   response.writeHead(200, {
-    'Set-cookie': ['yummy_cookie=choco', 'tasty-cookie=strawberry', `heart=good; Max-age=${60*60*24*30}`]
+    'Set-cookie': [
+      'yummy_cookie=choco',
+      'tasty-cookie=strawberry',
+      `heart=good; Max-age=${60 * 60 * 24 * 30}`,
+      `Secure=Secure; Secure`, //https에 대해서만 쿠키전송
+      'HttpOnly=HttpOnly; HttpOnly' //http타입에 대해서만 쿠키전송. 즉 js에서 안됨.
+
+    
+    ]
   });
   response.end('Cookie!!');
 }).listen(3000);
