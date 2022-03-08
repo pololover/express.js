8062958f23012fb453cdf249139fb630e75fd8bc
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-03-08 16:54:23 +0900

    글 수정 및 글 삭제
    <input type="hidden" name="A" value="B">일 때 submit을 누르면 A라는 이름에 B값을 추가하여 전송한다.

--- topic.js
+++ topic.js
@@ -5,7 +5,13 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 var auth = require('../lib/auth')
+var db = require('../lib/db');
+var shortid = require('shortid');
 router.get('/create', function (request, response) {  
+  if (!auth.IsOwner(request,response)) {
+    response.redirect('/');
+    return false;
+  }
   var title = 'WEB - create';
   var list = template.list(request.list);
   var html = template.HTML(title, list, `
@@ -23,23 +29,41 @@ router.get('/create', function (request, response) {
 });
 
 router.post('/create_process', function (request, response) {
+  if (!auth.IsOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
   var post = request.body;
   var title = post.title;
   var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.redirect(`/topic/${title}`);
-  });
+  id = shortid.generate();
+  db.get('topics').push({
+    id: id,
+    title: title,
+    description: description,
+    user_id: request.user.id
+  }).write();
+  response.redirect(`/topic/${id}`);
+  
 });
 
 router.get('/update/:pageId', function (request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
+  if (!auth.IsOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
+  var topic = db.get('topics').find({ id: request.params.pageId }).value();
+  if (topic.user_id !== request.user.id) {
+    request.flash('error', 'not yours');
+    return response.redirect('/');
+  }
+  var title = topic.title;
+  var description = topic.description;
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
       `
     <form action="/topic/update_process" method="post">
-    <input type="hidden" name="id" value="${title}">
+    <input type="hidden" name="id" value="${topic.id}">
     <p><input type="text" name="title" placeholder="title" value="${title}"></p>
     <p>
     <textarea name="description" placeholder="description">${description}</textarea>
@@ -49,63 +73,74 @@ router.get('/update/:pageId', function (request, response) {
     </p>
     </form>
     `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${topic.id}">update</a>`,
       auth.StatusUI(request, response)
     );
     response.send(html);
-  });
 });
 
 router.post('/update_process', function (request, response) {
+  if (!auth.IsOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
   var post = request.body;
   var id = post.id;
   var title = post.title;
   var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.redirect(`/topic/${title}`);
-    })
-  });
+  var topic = db.get('topics').find({ id: id }).value();
+  if (topic.user_id !== request.user.id) {
+    request.flash('error', 'not yours');
+    return response.redirect('/');
+  }
+
+  db.get('topics').find({ id: id }).assign({
+    title: title,
+    description: description
+  }).write();
+  response.redirect(`/topic/${topic.id}`);
 });
 
 router.post('/delete_process', function (request, response) {
-  if (request.isOwner == false) {
-    response.end('Login required!!');
-  } else {
-    console.log(request.isOwner);
+  if (!auth.IsOwner(request, response)) {
+    response.redirect('/');
+    return false;
+  }
     var post = request.body;
     var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function (error) {
-      response.redirect('/');
-    });
-  }
+    var topic = db.get('topics').find({ id: id }).value();
+    if (topic.user_id !== request.user.id) {
+      return response.redirect('/');
+    }
+    db.get('topics').remove({ id: id }).write();
+    response.redirect('/');
+    
 });
 
 router.get('/:pageId', function (request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-    <a href="/topic/update/${sanitizedTitle}">update</a>
+  var topic = db.get('topics').find({
+    id: request.params.pageId
+  }).value();
+  var user = db.get('users').find({
+    id: topic.user_id
+  }).value();
+  var sanitizedTitle = sanitizeHtml(topic.title);
+  var sanitizedDescription = sanitizeHtml(topic.description, {
+    allowedTags: ['h1']
+  });
+  var list = template.list(request.list);
+  var html = template.HTML(sanitizedTitle, list,
+    `<h2>${sanitizedTitle}</h2>${sanitizedDescription}
+     <p>by ${user.displayName}</p>
+    `,
+    ` <a href="/topic/create">create</a>
+    <a href="/topic/update/${topic.id}">update</a>
     <form action="/topic/delete_process" method="post">
-      <input type="hidden" name="id" value="${sanitizedTitle}">
+      <input type="hidden" name="id" value="${topic.id}">
       <input type="submit" value="delete">
     </form>`, auth.StatusUI(request, response)
       );
       response.send(html);
-    }
-  });
 });
 
 
